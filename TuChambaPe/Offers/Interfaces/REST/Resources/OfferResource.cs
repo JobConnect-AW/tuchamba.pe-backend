2c036bfd9217a900c131364de30852cae9dccae5
alejooroncoy <adoa2705@gmail.com>
2025-07-08 17:53:01 -0500

    fix: Remove id

--- TuChambaPe/Offers/Interfaces/REST/Resources/OfferResource.cs
+++ TuChambaPe/Offers/Interfaces/REST/Resources/OfferResource.cs
@@ -2,4 +2,4 @@ using TuChambaPe.Offers.Domain.Model.ValueObjects;
 
 namespace TuChambaPe.Offers.Interfaces.REST.Resources;
 
-public record OfferResource(int Id, Guid Uid, string Title, string Description, int CategoryId, float Amount, string Duration, string PaymentMethod, string Status,  int ProposalsCount, Guid? SelectedProposalUid, DateTimeOffset? StartAt); 
+public record OfferResource(Guid Uid, string Title, string Description, int CategoryId, float Amount, string Duration, string PaymentMethod, string Status,  int ProposalsCount, Guid? SelectedProposalUid, DateTimeOffset? StartAt); 
